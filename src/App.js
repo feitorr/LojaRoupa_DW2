9429c2eed2fc51f1404f23e036e19c2c2ad80900
Joao Belo <jobelo@deloitte.pt>
2024-04-11 15:39:58 +0100

    Header feito, só falta responsividade

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import NoPage from "./pages/NoPage";
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,25 +1,15 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+
+      <Header />
 
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Layout;
