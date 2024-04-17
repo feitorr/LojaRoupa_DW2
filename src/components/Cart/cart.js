239372cb8b5aa269c66f3b0ff75b556f32e6bc4a
Simão <simaobaenevesne1@gmail.com>
2024-04-17 07:05:10 -0400

    Carrinho&&ProductDetail

---
+++ src/components/Cart/cart.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./cart.css";
+
+const Cart = () => {
+
+    function closeCart() {
+        var cart = document.getElementById('popupCart');
+        cart.classList.remove('show');
+    }
+    
+    return (
+        <>
+            <div className="cart" id="popupCart">
+                <div className="popup-cart">
+                    <span className="close-btn" onClick={closeCart}>X</span>
+                    <h1>Carrinho</h1>
+                    <div id="cart-content">
+                    
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Cart;
