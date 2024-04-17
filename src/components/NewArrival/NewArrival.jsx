a5e8a737632e7ba2e3a94b5b6db19dd593fa0077
Simão <simaobaenevesne1@gmail.com>
2024-04-17 07:44:49 -0400

    FixCss

--- src/components/NewArrival/NewArrival.jsx
+++ src/components/NewArrival/NewArrival.jsx
@@ -7,32 +7,32 @@ const NewArrival = () => {
     <div class="allnew">
     <h1>Novos Lançamentos</h1>
       <div className="newarrival">
-        <div className="card">
-          <div className="cardimage">
+        <div className="cardd">
+          <div className="carddimage">
             <img src="https://i.postimg.cc/2ShXTvgn/61059-08-001-5000x-png.webp"></img>
           </div>
           <p className="category">Jacket/Homem</p>
           <h2>Casaco Carhart</h2>
           <p className="preco">80.00€</p>
         </div>
-        <div className="card">
-          <div className="cardimage">
+        <div className="cardd">
+          <div className="carddimage">
             <img src="https://i.postimg.cc/2ShXTvgn/61059-08-001-5000x-png.webp"></img>
           </div>
           <p className="category">Jacket/Homem</p>
           <h2>Casaco Carhart</h2>
           <p className="preco">80.00€</p>
         </div>
-        <div className="card">
-          <div className="cardimage">
+        <div className="cardd">
+          <div className="carddimage">
             <img src="https://i.postimg.cc/2ShXTvgn/61059-08-001-5000x-png.webp"></img>
           </div>
           <p className="category">Jacket/Homem</p>
           <h2>Casaco Carhart</h2>
           <p className="preco">80.00€</p>
         </div>
-        <div className="card">
-          <div className="cardimage">
+        <div className="cardd">
+          <div className="carddimage">
             <img src="https://i.postimg.cc/2ShXTvgn/61059-08-001-5000x-png.webp"></img>
           </div>
           <p className="category">Jacket/Homem</p>
