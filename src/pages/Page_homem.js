976a3768f32f93788dfe8da3132dfbdfe24853a1
Simão <simaobaenevesne1@gmail.com>
2024-05-08 05:05:49 -0400

    cartFix

--- src/pages/Page_homem.js
+++ src/pages/Page_homem.js
@@ -2,10 +2,12 @@ import Categoria_homem from "../components/Categorias/Categoria_homem";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import New_collection from "../components/New_collection/New_collection_homem";
+import Cart from "../components/Cart/cart";
 const Page_homem = () => {
   return (
     <>
       <Header />
+      <Cart />
       <Categoria_homem />
       <New_collection />
     </>
