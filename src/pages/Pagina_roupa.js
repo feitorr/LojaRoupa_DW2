00b72d6c3432a3076dd9d56ff1173a3b98a72b95
Admin <jfeitor14@gmail.com>
2024-05-09 12:35:45 +0100

    filtrar por categoria

--- src/pages/Pagina_roupa.js
+++ src/pages/Pagina_roupa.js
@@ -3,13 +3,19 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Filtragem from "../components/Filtragem/Filtragem";
 import Cart from "../components/Cart/cart";
+import Roupa_filtrada from "../components/Roupa_filtrada/Roupa_filtrada";
 
 const Pagina_roupa = () => {
   return (
     <>
         <Header />
         <Cart />
+        <div style={{ display: "flex", width:"100%"}} >
         <Filtragem />
+        <div style={{ textAlign:"center", width:"100%" }}>
+        <Roupa_filtrada/>
+        </div>
+        </div>
     </>
   );
 };
