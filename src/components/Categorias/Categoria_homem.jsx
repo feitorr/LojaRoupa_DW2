9011360df0a4ed1f317095db7c782bb8b8ea5dba
Admin <jfeitor14@gmail.com>
2024-06-01 22:15:34 +0100

    some bugs fixed

--- src/components/Categorias/Categoria_homem.jsx
+++ src/components/Categorias/Categoria_homem.jsx
@@ -13,31 +13,31 @@ const Categoria_homem = () => {
                 <h1>CATEGORIAS</h1>
                 <div className="flex">
                 <div className="categoria">
-                    <Link  class="link"to={`/roupa?categoria=${"T-shirt"}`}>
+                    <Link  class="link"to={`/roupa?categoria=${"T-shirt"}?genero=${"Homem"}`}>
                     <img src={tshirt} alt="T-SHIRTS" />
                     <p>T-Shirt</p>
                     </Link>
                 </div>
                 <div className="categoria">
-                <Link class="link" to={`/roupa?categoria=${"Sweats"}`}>
+                <Link class="link" to={`/roupa?categoria=${"Sweats"}?genero=${"Homem"}`}>
                     <img src={sweat} alt="SWEATS" />
                     <p>Sweats</p>
                     </Link> 
                 </div>
                 <div className="categoria">
-                <Link  class="link" to={`/roupa?categoria=${"Casacos"}`}>
+                <Link  class="link" to={`/roupa?categoria=${"Casacos"}?genero=${"Homem"}`}>
                     <img src={casao} alt="Casacos" />
                     <p>Jackets</p>
                     </Link>
                 </div>
                 <div className="categoria">
-                <Link  class="link" to={`/roupa?categoria=${"Jeans"}`}>
+                <Link  class="link" to={`/roupa?categoria=${"Jeans"}?genero=${"Homem"}`}>
                     <img src={jeans} alt="Calcas" />
                     <p>Jeans</p>
                     </Link>
                 </div>
                 <div className="categoria">
-                <Link  class="link" to={`/roupa?categoria=${"Chapeus"}`}>
+                <Link  class="link" to={`/roupa?categoria=${"Chapeus"}?genero=${"Homem"}`}>
                     <img src={chapeu} alt="Chapéus" />
                     <p>Hat</p>
                     
