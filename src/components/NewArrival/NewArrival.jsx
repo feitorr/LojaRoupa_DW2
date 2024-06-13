9772d683e13e6a15b341cb2c44dd9733b164be63
Simão <simaobaenevesne1@gmail.com>
2024-06-13 15:19:42 -0400

    NewArrival FIx

--- src/components/NewArrival/NewArrival.jsx
+++ src/components/NewArrival/NewArrival.jsx
@@ -14,18 +14,25 @@ const NewArrival = () => {
           .select("*")
           .eq('genero', 'Homem')
           .eq('estado', '1');
+
         if (error) {
           throw error;
         }
 
-        const selectedIndices = [];
-        while (selectedIndices.length < 4) {
-          const randomIndex = Math.floor(Math.random() * allRoupas.length);
-          if (!selectedIndices.includes(randomIndex)) {
-            selectedIndices.push(randomIndex);
-          }
+        if (allRoupas.length === 0) {
+          console.error("Nenhuma roupa encontrada.");
+          return;
         }
-        const randomRoupas = selectedIndices.map(index => allRoupas[index]);
+
+        const maxSelections = Math.min(4, allRoupas.length);
+        const selectedIndices = new Set();
+
+        while (selectedIndices.size < maxSelections) {
+          const randomIndex = Math.floor(Math.random() * allRoupas.length);
+          selectedIndices.add(randomIndex);
+        }
+
+        const randomRoupas = Array.from(selectedIndices).map(index => allRoupas[index]);
         setRoupas(randomRoupas);
       } catch (error) {
         console.error("Erro ao buscar roupas:", error.message);
@@ -40,30 +47,28 @@ const NewArrival = () => {
   };
 
   return (
-    <>
-      <div className="allnew">
-        <h1>Novos Lançamentos</h1>
-        <div className="newarrival">
-          {roupas.map((roupa, index) => (
-            <div className="cardd" key={index} onClick={() => handleLinkClick(roupa.id)}>
-              <div className="carddcontent">
-                <div className="carddimage">
-                  <img src={`https://lelwhxghwolrpmrkeeuw.supabase.co/storage/v1/object/public/imagens/${roupa.imagem}`} alt="Roupa" />
-                </div>
-                <p className="category">{roupa.categoria} / {roupa.marca}</p>
-                <h2 className="brand">{roupa.titulo}</h2>
-                <div className="precoDiv">
-                                    {roupa.promocao !== null && (
-                                        <p className="pricePromocaoo">{parseFloat(roupa.promocao).toFixed(2)}€</p>
-                                    )}
-                                    <p className="preco">{parseFloat(roupa.preco).toFixed(2)}€</p>
-                                </div>
+    <div className="allnew">
+      <h1>Novos Lançamentos</h1>
+      <div className="newarrival">
+        {roupas.map((roupa, index) => (
+          <div className="cardd" key={index} onClick={() => handleLinkClick(roupa.id)}>
+            <div className="carddcontent">
+              <div className="carddimage">
+                <img src={`https://lelwhxghwolrpmrkeeuw.supabase.co/storage/v1/object/public/imagens/${roupa.imagem}`} alt="Roupa" />
+              </div>
+              <p className="category">{roupa.categoria} / {roupa.marca}</p>
+              <h2 className="brand">{roupa.titulo}</h2>
+              <div className="precoDiv">
+                {roupa.promocao !== null && (
+                  <p className="pricePromocaoo">{parseFloat(roupa.promocao).toFixed(2)}€</p>
+                )}
+                <p className="preco">{parseFloat(roupa.preco).toFixed(2)}€</p>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
